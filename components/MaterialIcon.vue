56d85ba05beaa70d26e2f3000a4c9979336d62c2
Artem Netsvetaev <physphile@vk.com>
2023-02-11 11:20:26 +0300

    first commit

---
+++ components/MaterialIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <span class="material-symbols-sharp"> {{ icon }} </span>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ constants/index.js
@@ -0,0 +1,4 @@
+export const navItems = [
+    { title: 'Расписание', icon: 'calendar_month', path: '/timetable' },
+    { title: 'Сервисы', icon: 'apps', path: '/apps' },
+];
