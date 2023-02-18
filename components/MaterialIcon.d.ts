84be391462321b33af02352c5f96c5124fa95e6b
Artem Netsvetaev <physphile@vk.com>
2023-02-18 11:38:25 +0300

    add typings

---
+++ components/MaterialIcon.d.ts
@@ -0,0 +1,11 @@
+import type { DefineComponent } from "vue";
+
+interface Props {
+  icon: string;
+  size: number;
+  color: string;
+}
+
+declare const MaterialIcon: DefineComponent<Props>;
+
+export default MaterialIcon;
